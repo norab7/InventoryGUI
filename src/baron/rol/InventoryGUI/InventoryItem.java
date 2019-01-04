7f9f9c47436fbba12c070c434bf1ccf71377dc23
norabseven <stuart.baron@gmail.com>
2019-01-04 00:07:51 +0000

    Not working as external Jar

--- src/baron/rol/InventoryGUI/InventoryItem.java
+++ src/baron/rol/InventoryGUI/InventoryItem.java
@@ -6,18 +6,21 @@ import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
 public abstract class InventoryItem extends ItemStack {
-	private final UUID UID = UUID.randomUUID();
+	// private final UUID UID = UUID.randomUUID();
+	private ItemStack item;
 
-	public UUID getUID() {
-		return UID;
+	// public UUID getUID() {
+	// return UID;
+	// }
+
+	public ItemStack getItem() {
+		return item;
+	}
+
+	public void setItem(ItemStack item) {
+		this.item = item;
 	}
 
 	public abstract void action(ClickType c);
-
-	protected abstract void leftClick();
-
-	protected abstract void rightClick();
-
-	protected abstract void middleClick();
 
 }
